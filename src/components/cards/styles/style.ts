3165a26ede2d5e0fe76885c9ee6e902c319bcc14
Masha <parfenenkoma@gmail.com>
2021-02-11 23:01:39 +0300

    initiate project

---
+++ src/components/cards/styles/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  width: 400px;
+  height: 400px;
+  margin: auto;
+  background-color: gray;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ src/components/gql/queries/Character.query.ts
@@ -0,0 +1,22 @@
+import { gql } from '@apollo/client';
+
+const CHARACTERS = gql`
+  query Characters {
+    characters {
+      info {
+        count
+      }
+      results {
+        id
+        name
+        species
+        location {
+          name
+        }
+        image
+      }
+    }
+  }
+`;
+
+export {CHARACTERS}
