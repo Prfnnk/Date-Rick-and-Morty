be86199cdf377f26729590ff60086ce492fc669f
Masha <parfenenkoma@gmail.com>
2021-02-18 23:14:22 +0300

    add func for cards removing

--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,69 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { CHARACTERS } from '../gql/queries/Character.query';
 import { Characters } from '../gql/queries/types/Characters';
-import { useSprings, animated, interpolate } from 'react-spring';
-import { useGesture } from 'react-use-gesture';
-import { Card } from './styles/style';
-
-const to = (i: number) => ({ x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 20, delay: i * 100 });
-const from = (i: number) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
-// This is being used down there in the view, it interpolates rotation and scale into a css transform
-const trans = (r: number, s: any) =>
-  `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`;
+import { useSpring } from 'react-spring';
+import { Card, ButtonsBlock, LikeBtn, SkipBtn, Wrapper, CardsBlock, Inner } from './styles/style';
 
 const Cards = () => {
   const { loading, error, data } = useQuery<Characters>(CHARACTERS);
   const results = data?.characters?.results;
+  const [toggle, setToggle] = useState(false);
+  const [id, setId] = useState(null);
+  const props = useSpring({ opacity: toggle ? 0 : 1 });
+  const [cards, setCards] = useState([]);
+  const [ok, setOk] = useState(false);
 
-  const cards =
-    results?.map((item) => {
-      return item?.image;
-    }) || [];
+  useEffect(() => {
+    if (results) {
+      setCards(results);
+      setId(results[0].id);
+      setOk(true);
+    }
+  }, [results]);
 
-  console.log(cards);
+  useEffect(() => {
+    if (cards && results && ok) {
+      if (cards.length < results.length) {
+        setId(cards[0].id);
+      }
+    }
+  }, [cards, results, ok]);
 
-  //animation
-  const [gone] = useState(() => new Set()); // The set flags all the cards that are flicked out
-  const [props, set] = useSprings(cards.length, (i) => ({ ...to(i), from: from(i) })); // Create a bunch of springs using the helpers above
-  // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
-  const bind = useGesture(({ args: [index], down, delta: [xDelta], distance, direction: [xDir], velocity }) => {
-    const trigger = velocity > 0.2; // If you flick hard enough it should trigger the card to fly out
-    const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
-    if (!down && trigger) gone.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
-    set((i: any) => {
-      if (index !== i) return; // We're only interested in changing spring-data for the current spring
-      const isGone = gone.has(index);
-      const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
-      const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
-      const scale = down ? 1.1 : 1; // Active cards lift up a bit
-      return { x, rot, scale, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } };
-    });
-    if (!down && gone.size === cards.length) setTimeout(() => gone.clear() || set((i: any) => to(i)), 600);
-  });
+  const removeCard = () => {
+    const filtered = cards.filter((item) => item.id !== id);
+    setCards(filtered);
+  };
 
-  //
+  console.log(cards, 'cards', results, 'res', data, 'data');
 
-  if (loading) return <p>Loading...</p>;
+  if (loading && !ok) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
   return (
-    <div className="Wrapper">
-      {results &&
-        results.map((item) => {
-          if (item === null) {
-            return null;
-          }
-          return (
-            <Card key={item.id}>
-              <div className="pic">
-                <img src={item.image ?? ''} alt="" />
-                <div className="name">{item.name ?? '-'}</div>
-              </div>
-            </Card>
-          );
-        })}
-    </div>
+    <Wrapper>
+      <Inner>
+        <CardsBlock>
+          {cards &&
+            cards
+              .map((item) => {
+                if (item === null) {
+                  return null;
+                }
+                return (
+                  <>
+                    <Card key={item.id} active={item.id === id}>
+                      <div className="pic">
+                        <img src={item.image ?? ''} alt="" />
+                        <div className="name">{item.name ?? '-'}</div>
+                        <div className="species">{item.species ?? ''}</div>
+                        <div className="location">{item.location.name ?? ''}</div>
+                      </div>
+                    </Card>
+                  </>
+                );
+              })
+              .reverse()}
+        </CardsBlock>
+        <ButtonsBlock>
+          <SkipBtn onClick={() => removeCard()}></SkipBtn>
+          <LikeBtn onClick={() => setToggle(true)}></LikeBtn>
+        </ButtonsBlock>
+      </Inner>
+    </Wrapper>
   );
 };
 export default Cards;
--- src/components/cards/styles/style.ts
+++ src/components/cards/styles/style.ts
@@ -1,13 +1,83 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { animated as a } from 'react-spring';
+import heartInitial from '../images/heart-initial.svg';
+import skip from '../images/skip-initial.svg';
+import heartOnTap from '../images/heart-on-tap.svg';
+import skipOnTap from '../images/skip-on-tap.svg';
 
-export const Card = styled.div`
-  width: 400px;
-  height: 400px;
-  margin: auto;
-  background-color: gray;
-  border-radius: 10px;
-  margin-bottom: 10px;
+type CardProps = {
+  active: boolean;
+};
+
+export const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+export const Inner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const CardsBlock = styled.div`
+  position: relative;
+  width: 400px;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Card = styled.div<CardProps>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 400px;
+  height: 400px;
+  background-color: gray;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Btn = css`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  cursor: pointer;
+
+  &:active {
+    transform: scale(1.1);
+  }
+`;
+
+export const ButtonsBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 150px;
+  margin-top: 20px;
+`;
+export const SkipBtn = styled.div`
+  ${Btn};
+  background-image: url('${skip}');
+  &:hover {
+    background-image: url('${skipOnTap}');
+  }
+`;
+
+export const LikeBtn = styled.div`
+  ${Btn};
+  background-image: url('${heartInitial}');
+  &:hover {
+    background-image: url('${heartOnTap}');
+  }
 `;
