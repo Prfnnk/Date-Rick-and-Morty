32e6750a2e77ebdaa10b04b6d5ac42077606c9e1
Masha <parfenenkoma@gmail.com>
2021-03-02 22:40:57 +0300

    add checkboxes, input

--- src/components/fiter-block/index.tsx
+++ src/components/fiter-block/index.tsx
@@ -1,7 +1,50 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { FilterItem, FilterBody, Title, ItemTitle, Input, Checkbox, CheckboxItem, Label } from './styles/style';
 
 const FilterBlock = () => {
-  return <div className="kek">kek</div>;
+  return (
+    <FilterBody>
+      <Title>Выберите фильтры для поиска</Title>
+      <FilterItem>
+        <ItemTitle>by name</ItemTitle>
+        <Input></Input>
+      </FilterItem>
+      <FilterItem>
+        <ItemTitle>by species</ItemTitle>
+        <CheckboxItem>
+          <Checkbox type="checkbox" id="human" value="human"></Checkbox>
+          <Label htmlFor="human">Human</Label>
+        </CheckboxItem>
+        <CheckboxItem>
+          <Checkbox type="checkbox" id="alien" value="alien"></Checkbox>
+          <Label htmlFor="alien">Alien</Label>
+        </CheckboxItem>
+        <CheckboxItem>
+          <Checkbox type="checkbox" id="mc" value="mc"></Checkbox>
+          <Label htmlFor="mc">Mythological Creature</Label>
+        </CheckboxItem>
+      </FilterItem>
+      <FilterItem>
+        <ItemTitle>by gender</ItemTitle>
+        <CheckboxItem>
+          <Checkbox type="checkbox" id="female" value="female"></Checkbox>
+          <Label htmlFor="female">female</Label>
+        </CheckboxItem>
+        <CheckboxItem>
+          <Checkbox type="checkbox" id="male" value="male"></Checkbox>
+          <Label htmlFor="male">male</Label>
+        </CheckboxItem>
+        <CheckboxItem>
+          <Checkbox type="checkbox" id="genderless" value="genderless"></Checkbox>
+          <Label htmlFor="genderless">genderless</Label>
+        </CheckboxItem>
+        <CheckboxItem>
+          <Checkbox type="checkbox" id="unknown" value="unknown"></Checkbox>
+          <Label htmlFor="unknown">unknown</Label>
+        </CheckboxItem>
+      </FilterItem>
+    </FilterBody>
+  );
 };
 export default FilterBlock;
--- src/components/fiter-block/styles/style.ts
+++ src/components/fiter-block/styles/style.ts
@@ -1,1 +1,69 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+export const FilterBody = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 300px;
+  height: 100vh;
+  background-color: white;
+  box-shadow: 0 0 10px rgb(0 0 0 / 20%);
+  padding: 0 20px;
+`;
+export const FilterItem = styled.div`
+  font-family: 'Roboto', sans-serif;
+  font-size: 22px;
+  font-weight: 400;
+  text-align: left;
+  width: 100%;
+  padding: 20px 0;
+  display: flex;
+  flex-direction: column;
+`;
+export const ItemTitle = styled.div`
+  margin-bottom: 15px;
+`;
+export const Title = styled(FilterItem)`
+  border-bottom: 2px solid grey;
+  font-weight: 700;
+`;
+export const Input = styled.input`
+  width: 100%;
+  height: 30px;
+  font-size: 18px;
+  padding: 5px;
+`;
+
+export const Label = styled.label`
+  padding-left: 25px;
+  position: relative;
+
+  &:after {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: calc(50% - 8px);
+    width: 13px;
+    height: 13px;
+    border: 1px solid black;
+  }
+`;
+
+export const Checkbox = styled.input`
+  display: none;
+
+  &:checked + ${Label}:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: calc(50% - 8px);
+    width: 15px;
+    height: 15px;
+    background: red;
+  }
+`;
+export const CheckboxItem = styled.div`
+  display: flex;
+  font-size: 18px;
+  margin-bottom: 10px;
+`;
